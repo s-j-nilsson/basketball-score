96ec5bd8c4164230294efff6cc9b2c3785c1c4c4
Johan Nilsson <s.j.nilsson@gmail.com>
2026-04-28 17:49:42 +0200

    Add more spacing to the share text to improve readability

--- app.js
+++ app.js
@@ -60,7 +60,7 @@
         share.onclick = async () => {
           const title = game.period===config.numPeriods ? 'Slutresultat' : 'Period '+game.period;
           await navigator.share({ text: `${title}
-${config.team1} ${total('a')} - ${total('b')} ${team2.value}` });
+${config.team1}     ${total('a')} - ${total('b')}     ${team2.value}` });
         };
 
         newGame.onclick = () => { if(confirm('Starta ny match?')){ disableWakeLock(); location.reload(); } };
